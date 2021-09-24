7fc3b339a381c549482c954ecc05f9393a440861
Otavio Sousa <otavioklearskin@gmail.com>
2021-09-24 10:21:57 -0300

    feat: leaflet integration, create map component and dynamic import, add map in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import ListItem from '../components/listItem';
+import Map from '../components/Map';
+
+const DEFAULT_CENTER = [38.907132, -77.036546];
 
 export default function Home() {
   return (
@@ -11,14 +13,20 @@ export default function Home() {
         <meta name="description" content="Setup with nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <img src="/assets/robotics.png" alt="robot" />
-          <h1>Hello world!</h1>
-        </div>
-        <div>
-          <ListItem />
-        </div>
+      <main className={styles.main}>
+        <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
+          {({ TileLayer, Marker, Popup }) => (
+            <>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <Marker position={DEFAULT_CENTER}>
+                <Popup>A pretty CSS3 popup</Popup>
+              </Marker>
+            </>
+          )}
+        </Map>
       </main>
     </div>
   );
