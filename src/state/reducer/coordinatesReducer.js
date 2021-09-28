9674f4db90d6e27908200f047f3c93e39cc33827
Otavio Sousa <otavioklearskin@gmail.com>
2021-09-28 09:13:26 -0300

    fix: change data from initial coordinates state

--- src/state/reducer/coordinatesReducer.js
+++ src/state/reducer/coordinatesReducer.js
@@ -1,11 +1,11 @@
 const INITIAL_STATE = {
-  data: [38.907132, -77.036546],
+  data: [-23.4009, -51.9678],
 };
 
 function coordinatesReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'SET_COORDINATES':
-      return { ...state, data: [action.data] };
+      return { ...state, data: action.data };
     default:
       return state;
   }
