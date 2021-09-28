3254eaca40f292bcf888f9904a4066f07c6aa57a
Otavio Sousa <otavioklearskin@gmail.com>
2021-09-28 16:58:34 -0300

    feat: define map and separete map component from homepage

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,41 @@
-import dynamic from 'next/dynamic';
+import React from 'react'
+import { useSelector } from 'react-redux';
+import styles from './styles.module.css';
+import DefineMap from './define'
 
-const Map = dynamic(() => import('./Map'), {
-  ssr: false,
-});
+export default function Map(){
 
-export default Map;
+    const coords = useSelector((state) => state.coordinatesReducer.data);
+    const multiPolygon = useSelector((state) => state.polygonReducer.data);
+
+    return (
+        <DefineMap className={styles.homeMap} center={coords} zoom={12}>
+          {({ TileLayer, Marker, Popup, Polygon }) => (
+            <>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+
+              {multiPolygon.length > 0 && multiPolygon.map((polygon, index) => {
+                return (
+                  <Polygon 
+                    pathOptions={{ color: 'purple' }} 
+                    positions={[polygon.geometry.coordinates[0][0].map(coordinates => {
+                      return [coordinates[1], coordinates[0]]
+                    })]}
+                    key={index}
+                  >
+                    <Popup>{`
+                      População: ${polygon.properties.population}
+                      Renda média: ${polygon.properties.averageincome}
+                    `}</Popup>
+                  </Polygon>
+                )
+              })}
+            </>
+          )}
+        </DefineMap>
+    )
+}
+
