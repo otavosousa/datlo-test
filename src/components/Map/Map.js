e509723e5298f4185bf5324122cf6467a3df3091
Otavio Sousa <otavioklearskin@gmail.com>
2021-09-28 09:11:29 -0300

    feat: add  set view map handle

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import L from 'leaflet';
 import * as ReactLeaflet from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import { useMap } from 'react-leaflet'
+import { useSelector } from 'react-redux';
 import styles from './styles.module.css';
-
 const { MapContainer } = ReactLeaflet;
 
 const Map = ({ children, className, ...rest }) => {
+  const coords = useSelector((state) => state.coordinatesReducer.data);
   let mapClassName = styles.map;
 
   if (className) {
@@ -24,8 +25,15 @@ const Map = ({ children, className, ...rest }) => {
     });
   }, []);
 
+  function SetViewOnClick({ coords }) {
+    const map = useMap();
+    map.panTo(coords, 30)
+    return null;
+  }
+
   return (
     <MapContainer className={mapClassName} {...rest}>
+      <SetViewOnClick coords={coords} />
       {children(ReactLeaflet)}
     </MapContainer>
   );
